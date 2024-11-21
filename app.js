5e9c8435a72b8f052b7b355bec1e8cd9e39e28e5
szt3rackev <szt3rackev@SZT5-9-HP>
2024-11-21 15:57:26 +0100

    Adatbázis létrehozott!

--- app.js
+++ app.js
@@ -5,23 +5,42 @@ import { fileURLToPath } from 'url';
 const app = express();
 app.use(express.json());
 
+const users = [
+    {
+        id: 1,
+        name: "Adel"
+    },
+    {
+        id: 2,
+        name: "Beata"
+    },
+    {
+        id: 3,
+        name: "Cecil"
+    }
+];
+
 app.get("/hello", (req, res, next) => {
     res.send("Hello user!");
-})
+});
 
 app.get("/greet", (req, res, next) => {
     res.send("Hi there!");
-})
+});
 
 app.get("/ciao", (req, res, next) => {
     res.send("Ciao on my page!");
-})
+});
 
 const root = path.join(path.dirname(fileURLToPath(import.meta.url)))
 
 app.get("/", (req, res, next) => {
     res.sendFile(path.join(root, 'index.html'));
-})
+});
+
+app.get("/users", (req, res) => {
+    res.send(JSON.stringify(users));
+});
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
